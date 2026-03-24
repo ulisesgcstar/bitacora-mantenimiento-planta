f08426cbec2deb0d1e72e41a510f9356a38c5bbe
Arket <ulisesgcmarketing@gmail.com>
2026-03-24 10:22:04 -0600

    feat: conexion verificada

---
+++ src/main.js
@@ -0,0 +1,4 @@
+function saludarTomza() {
+  console.log("¡Conexión exitosa desde WSL para el sistema de bitácoras!");
+  SpreadsheetApp.getActiveSpreadsheet().toast("🚀 ¡Sistema vinculado con éxito!");
+}
