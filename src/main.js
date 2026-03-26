4a16ff58bdb7043af8f05cd7cce34725aaf278b5
Arket <ulisesgcmarketing@gmail.com>
2026-03-25 18:12:35 -0600

    chore: clean src files for Blueprint v2.0 architecture

--- src/main.js
+++ src/main.js
@@ -1,4 +1,1 @@
-function saludarTomza() {
-  console.log("¡Conexión exitosa desde WSL para el sistema de bitácoras!");
-  SpreadsheetApp.getActiveSpreadsheet().toast("🚀 ¡Sistema vinculado con éxito!");
-}
+
